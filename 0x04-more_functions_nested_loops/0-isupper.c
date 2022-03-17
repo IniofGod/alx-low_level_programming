c6c0289b76847697bb2402f3b3a5475c5c98a134
IniofGod <blessingilesanmi22@gmail.com>
2022-03-17 20:54:30 +0000

    Checks for uppercase character

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,15 @@
 #include "holberton.h"
 /**
- *_isupper - checks if letters are uppercase.
- *@c: input.
- *
- *Return: 1 if c is uppercase
- *0 otherwise.
+ * _isupper - function that checks for uppercase character.
+ * @c: the int for the paramaters of my function
+ * Return: 1 if upper, else 0
  */
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 65 && c <= 90)
 	{
-		return (1);
+	return (1);
 	}
 	else
-	{
-		return (0);
-	}
-}[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cmain[[C[C[C[C[C[C[C[C[C[C[C[C[C[Cmain.h"[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+	return (0);
+}[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[C[A[C[C[C[C[C[C[Cmain.h[C[C[C[C"[B[B[D[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D[D[D[B[B[B[D[D[D[D[D
