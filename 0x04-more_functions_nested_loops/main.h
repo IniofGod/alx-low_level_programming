a2dafadc078f9e1d59e221567ebb1a66f47aea1d
IniofGod <blessingilesanmi22@gmail.com>
2022-03-17 20:52:31 +0000

    Update main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,16 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-#include <stdio.h>
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char c);
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
-void  print_remaining_days(int month, int day, int year);
-
-#endif /* HOLBERTON_H */[D[DMAIN_H */[C[C[C[A[A[A[A[A[A[A[AMAIN_H[C[C[C[C[AMAIN_H[C[C[C[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C
+#endif[A[A[A[A[A[A[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C[CMAIN_H[C[C[C[A[C[C[MAIN_H[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[D[D[D[D[D[D[D[D
