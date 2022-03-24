c92fadb0e65feb447acdb5b621fd3ab4059e4d37
IniofGod <blessingilesanmi22@gmail.com>
2022-03-24 20:23:42 +0000

    Prints a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,38 +1,58 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * print_line - prints a s bytes of a buffer
+ * @c: buffer to print
+ * @s: bytes of buffer to print
+ * @l: line of buffer to print
+ *
+ * Return: void
+ */
+
+void print_line(char *c, int s, int l)
+{
+int j, k;
+for (j = 0; j <= 9; j++)
+{
+if (j <= s)
+printf("%02x", c[l * 10 + j]);
+else
+printf("  ");
+if (j % 2)
+putchar(' ');
+}
+for (k = 0; k <= s; k++)
+{
+if (c[l * 10 + k] > 31 && c[l * 10 + k] < 127)
+putchar(c[l * 10 + k]);
+else
+putchar('.');
+}
+}
 
 /**
-* print_buffer - prints a buffer.
-* @b: pointer to string
-* @size: total number of bytes
-* Return: nothing
-**/
-
+ * print_buffer - prints a buffer
+ * @b: buffer to print
+ * @size: size of buffer
+ *
+ * Return: void
+ */
 void print_buffer(char *b, int size)
 {
-	int c, d;
-
-	c = 0;
-	if (size <= 0)
-	{
-		printf("\n");
-	}
-
-	while (c < size)
-	{
-		printf("%8.8x:", c);
-		d = 0;
-		while (d < 10 )
-		{
-			printf("%02x", b[c + d]);
-			if ((d % 2 == 0 && d != 0) || (c + d > size - 1))
-			{
-				printf(" ");
-			}
-			d++;
-		}
-		c += 10;
-		printf("\n");
-	}
-
+int i;
+for (i = 0; i <= (size - 1) / 10 && size; i++)
+{
+printf("%08x: ", i * 10);
+if (i < size / 10)
+{
+print_line(b, 9, i);
+}
+else
+{
+print_line(b, size % 10 - 1, i);
+}
+putchar('\n');
+}
+if (size == 0)
+putchar('\n');
 }
