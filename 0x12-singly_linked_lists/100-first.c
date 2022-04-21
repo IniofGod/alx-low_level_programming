14af18f58160247a1fa74aa2c5c9e7eee06cb378
IniofGod <blessingilesanmi22@gmail.com>
2022-04-21 22:26:44 +0100

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
 
+/* Using constructors in C */
+void main_constructor(void) __attribute__((constructor));
 /**
-* printFirst - a func that prints You're beat!
-* and yet, you must allow,\nI bore my house upon
-* my back!\n before the main function is executed
-*/
-
-void printFirst(void)
+ * main_constructor - A constructor function that prints a message
+ * before main runs
+ * Return: Nothing
+ */
+void main_constructor(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
