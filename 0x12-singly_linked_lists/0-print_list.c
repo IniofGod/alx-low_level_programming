60befd93b27f12e204a0fadc587d03ba92622f0e
IniofGod <blessingilesanmi22@gmail.com>
2022-04-21 22:22:16 +0100

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,25 +1,28 @@
-#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
 /**
- * print_list - A function that prints a linked list
- * @h: pointer t  head of list
- * Return: number of nodes in list as size_t
- */[A[A[C[C[C[C[A[A[C[C[C[C[C[C[C[C[C[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C Print a linked list[C[C[C[C[C[C[C[C[C[C[C[C
-[B[B[C[C[C
+* print_list - prints all the elements of a linked_list
+* @h: a linked_list
+* Return: a structure to a linked_list
+**/
+
 size_t print_list(const list_t *h)
 {
-	unsigned int nodes = 0;
+	unsigned int size;
+
+	size = 0;
 
 	while (h)
 	{
-		printf("[%u] ", h->len);
 		if (h->str == NULL)
-			printf("(nil)\n");
+			printf("[0] (nil)\n");
 		else
-			printf("%s\n", h->str);
-
+			printf("[%i] %s\n", h->len, h->str);
 		h = h->next;
-		nodes++;
+		size += 1;
 	}
-	return (nodes);
+
+	return (size);
 }
