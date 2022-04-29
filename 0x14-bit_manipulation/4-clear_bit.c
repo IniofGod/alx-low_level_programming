ad9bad21d065ef2310f6c707e1a7ee376c1eea8a
IniofGod <blessingilesanmi22@gmail.com>
2022-04-29 04:54:55 +0100

    Set the value of a bit to 0 at a given index

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,40 +1,22 @@
-#include  "main.h"
-/**
-  *getp - returns the value of x to the power of y.
-  *@x: number.
-  *@y: power.
-  *
-  *Return: x to the power of y.
-  */
-int getp(int x, int y)
-{
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-	if (y == 1)
-		return (x);
-	return (x * getp(x, y - 1));
-}
+#include "main.h"
 
 /**
-  *clear_bit - stes the value of a bit to one at a given index.
-  *@n: pointer to number.
-  *@index: index of bit.
-  *
-  *Return: 1 if it worked, -1 if it did not.
-  */
+ * clear_bit - clear bit to 0 at given index
+ * @n: number
+ * @index: index within binary number
+ * Return: 1 if success, or -1 if error
+ */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > 63)
+	unsigned long int max_bits;
+	unsigned long int mask = 1;
+
+	max_bits = (sizeof(unsigned long int) * 8);
+	if (index > max_bits)
 		return (-1);
-	if (n)
-	{
-		*n -= getp(2, index);
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+
+	mask = ~(mask << index);
+	*n = (*n & mask);
+
+	return (1);
 }
